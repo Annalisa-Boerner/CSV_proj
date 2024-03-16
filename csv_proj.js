d6a5e896223b5c1d7eb3abc67147423f1745819f
Annalisa Boerner <annalisa.boerner@gmail.com>
2024-03-16 11:59:45 -0400

    added logic for filtering through countries that exist and adding countires that don't

--- csv_proj.js
+++ csv_proj.js
@@ -6,10 +6,13 @@ const results = [];
 
 const directoryName = "/root/csv_proj/data/GLJ/Destinations-of-Silk-2022"
 const fileName = "Destinations-of-Silk-2022.csv"
-// Path to your Excel file
+// Path to Excel file
 const filePath = path.join(directoryName, fileName);
 
 console.log("filepath", filePath)
+
+//Global countries variable to be populated as we loop through
+const countries = []
 
 fs.createReadStream(filePath)
     .pipe(csv())
@@ -17,18 +20,38 @@ fs.createReadStream(filePath)
     .on('end', () => {
         // Output the data
         // console.log(results);
-        const countries = []
         const tradeValues = []
 
         for (let i = 0; i < results.length; i++) {
+            let country = countries.filter((c) => {
+                return c["name"] = results[i]["Country"]
+            })
+            if (country) {
+                country = country[0]
+            } else {
+                let country = {
+                    "name": results[i]["Country"],
+                    "products": []
+                }
+                countries.push(country)
+            }
+
             countries.push(results[i]["Country"])
+
             tradeValues.push(results[i]["Trade Value"])
 
         }
+        //TODO review how to .map() this function instead as well
+
 
         console.log('countries, populated:', countries)
         console.log('tradeValues, populated:', tradeValues)
     });
 
-// console.log('results array', results)
+
+//CSV target for write at the end
+const newFilePath = "/root/csv_proj/data/aggregatedFigures1.csv"
+
+
+
 
